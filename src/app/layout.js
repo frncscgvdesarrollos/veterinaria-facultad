65fe9884435a70b394db8c32dcde94ff4c9e7d49
frncscgvdesarrollos <frncscgvdesarrollos@gmail.com>
2025-10-13 01:48:21 +0000

    fix(tailwind): Repair CSS compilation by correcting theme and layout setup

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,3 @@
-
 import "./globals.css";
 import { AuthProvider } from "@/contexts/AuthContext";
 import Header from "@/app/components/Header";
@@ -6,7 +5,7 @@ import Header from "@/app/components/Header";
 export default function RootLayout({ children }) {
   return (
     <html lang="es" suppressHydrationWarning>
-      <body className={`bg-gray-50 flex flex-col min-h-screen`}>
+      <body className={`bg-background text-foreground flex flex-col min-h-screen`}>
         <AuthProvider>
           <Header />
           <main className="flex-grow w-full">
