16ba12c774c559224e2dc437c33d59e3fd454bf7
frncscgvdesarrollos <frncscgvdesarrollos@gmail.com>
2025-10-30 14:27:19 +0000

    fecha horario clinica form turno

--- src/lib/actions/turnos.actions.js
+++ src/lib/actions/turnos.actions.js
@@ -2,7 +2,7 @@
 
 import admin from '@/lib/firebaseAdmin';
 import { revalidatePath } from 'next/cache';
-import { verificarDisponibilidadTraslado } from '../logica_traslado';
+import { verificarDisponibilidadTraslado } from '../logica_traslado.js';
 import dayjs from 'dayjs';
 import utc from 'dayjs/plugin/utc';
 import timezone from 'dayjs/plugin/timezone';
